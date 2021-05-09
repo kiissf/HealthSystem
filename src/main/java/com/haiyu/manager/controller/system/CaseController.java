2f68de7e8f38d792f062d971378a748feb968b0c
Lshifan <599671256@qq.com>
2021-05-09 18:17:45 +0800

    qub

--- src/main/java/com/haiyu/manager/controller/system/CaseController.java
+++ src/main/java/com/haiyu/manager/controller/system/CaseController.java
@@ -11,7 +11,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
 /**
  * @ClassName CaseController
